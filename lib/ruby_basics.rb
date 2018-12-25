dc82195d2ebef0e25f8b30bc7b3a0a21c4c3c210
Heloise Taillet <ht2394@columbia.edu>
2018-12-25 02:35:34 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -6,22 +6,22 @@ def assign_variable(value)
   variable = value
 end
 
-def argue
-  
+def argue(ment)
+ment
 end
 
-def greeting
+def greeting(one, two)
 
 end
 
 def return_a_value
-
+puts "Nice"
 end
 
 def last_evaluated_value
-
+puts "expert"
 end
 
-def pizza_party
-
+def pizza_party(argument = "cheese")
+argument
 end
